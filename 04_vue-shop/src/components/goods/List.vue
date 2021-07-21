f85ea25577870b7beae9aad9be83d772b12d5ccb
543807939@qq.com <543807939@qq.com>
2021-07-21 23:16:02 +0800

    study

--- 04_vue-shop/src/components/goods/List.vue
+++ 04_vue-shop/src/components/goods/List.vue
@@ -11,8 +11,17 @@
     <el-card>
       <el-row :gutter="20">
         <el-col :span="8">
-          <el-input placeholder="请输入内容" @clear="search" v-model="queryInfo.query" clearable>
-            <el-button slot="append" icon="el-icon-search" @click="search"></el-button>
+          <el-input
+            placeholder="请输入内容"
+            @clear="search"
+            v-model="queryInfo.query"
+            clearable
+          >
+            <el-button
+              slot="append"
+              icon="el-icon-search"
+              @click="search"
+            ></el-button>
           </el-input>
         </el-col>
         <el-col :span="4">
@@ -23,14 +32,29 @@
       <el-table :data="goodsList" border stripe>
         <el-table-column type="index" label="#"></el-table-column>
         <el-table-column label="商品名称" prop="goods_name"></el-table-column>
-        <el-table-column width="105px" label="商品价格(元)" prop="goods_price"></el-table-column>
-        <el-table-column width="80px" label="商品重量" prop="goods_weight"></el-table-column>
+        <el-table-column
+          width="105px"
+          label="商品价格(元)"
+          prop="goods_price"
+        ></el-table-column>
+        <el-table-column
+          width="80px"
+          label="商品重量"
+          prop="goods_weight"
+        ></el-table-column>
         <el-table-column width="170px" label="创建时间" prop="upd_time ">
-          <template slot-scope="scope">{{scope.row.upd_time | dateFormat}}</template>
+          <template slot-scope="scope">{{
+            scope.row.upd_time | dateFormat
+          }}</template>
         </el-table-column>
         <el-table-column width="120px" label="操作">
           <template slot-scope="scope">
-            <el-button icon="el-icon-edit" size="mini" type="primary"></el-button>
+            <el-button
+              icon="el-icon-edit"
+              size="mini"
+              type="primary"
+              @click="showEditDialog(scope.row.goods_id)"
+            ></el-button>
             <el-button
               icon="el-icon-delete"
               size="mini"
@@ -51,6 +75,29 @@
         :total="total"
         background
       ></el-pagination>
+      <!-- 编辑弹窗区域 -->
+      <el-dialog
+        title="编辑商品"
+        :visible.sync="editDialogVisible"
+        width="50%"
+        @close="editDialogClose"
+      >
+        <el-form :model="editGood" :rules="editGoodRules" ref="editGoodRef">
+          <el-form-item label="商品名称" prop="goods_name">
+            <el-input v-model="editGood.goods_name"></el-input>
+          </el-form-item>
+          <el-form-item label="商品价格" prop="goods_price">
+            <el-input v-model="editGood.goods_price" type="number"></el-input>
+          </el-form-item>
+          <el-form-item label="商品重量" prop="goods_weight">
+            <el-input v-model="editGood.goods_weight" type="number"></el-input>
+          </el-form-item>
+        </el-form>
+        <span slot="footer" class="dialog-footer">
+          <el-button @click="editDialogVisible = false">取 消</el-button>
+          <el-button type="primary" @click="submitEditGood">确 定</el-button>
+        </span>
+      </el-dialog>
     </el-card>
     <router-view></router-view>
   </div>
@@ -60,7 +107,7 @@ export default {
   data() {
     return {
       queryInfo: {
-        query: '',
+        query: "",
         pagenum: 1,
         pagesize: 10,
       },
@@ -68,62 +115,115 @@ export default {
       goodsList: [],
       // 总数据条数
       total: 0,
-    }
+      // 编辑弹窗是否显示
+      editDialogVisible: false,
+      // 编辑的商品
+      editGood: {},
+      // 编辑商品验证规则
+      editGoodRules: {
+        goods_name: [
+          { required: true, message: "请输入商品名称", trigger: "blur" },
+          {
+            min: 2,
+            max: 20,
+            message: "长度在 2 到 20 个字符",
+            trigger: "blur",
+          },
+        ],
+        goods_price: [
+          { required: true, message: "请输入商品名称", trigger: "blur" },
+        ],
+        goods_weight: [
+          { required: true, message: "请输入商品名称", trigger: "blur" },
+        ],
+      },
+    };
   },
   methods: {
     async getGoodsList() {
-      let { data: res } = await this.$http.get('goods', {
+      let { data: res } = await this.$http.get("goods", {
         params: this.queryInfo,
-      })
+      });
       if (res.meta.status !== 200) {
-        return this.$message.error('获取商品列表失败!')
+        return this.$message.error("获取商品列表失败!");
       }
-      console.log(res)
-      this.goodsList = res.data.goods
-      this.total = res.data.total
+      this.goodsList = res.data.goods;
+      this.total = res.data.total;
     },
     handleSizeChange(newSize) {
-      this.queryInfo.pagesize = newSize
-      this.getGoodsList()
+      this.queryInfo.pagesize = newSize;
+      this.getGoodsList();
     },
     handleCurrentChange(newNum) {
-      this.queryInfo.pagenum = newNum
-      this.getGoodsList()
+      this.queryInfo.pagenum = newNum;
+      this.getGoodsList();
     },
     search() {
-      this.getGoodsList()
+      this.getGoodsList();
     },
     // 删除商品
     async deleteGoods(id) {
       let confirmResult = await this.$confirm(
-        '此操作将永久删除该商品, 是否继续?',
-        '提示',
+        "此操作将永久删除该商品, 是否继续?",
+        "提示",
         {
-          confirmButtonText: '确定',
-          cancelButtonText: '取消',
-          type: 'warning',
+          confirmButtonText: "确定",
+          cancelButtonText: "取消",
+          type: "warning",
         }
-      ).catch((err) => err)
-      if (confirmResult == 'cancel') {
-        return
+      ).catch((err) => err);
+      if (confirmResult == "cancel") {
+        return;
       }
       // 确认删除 执行删除操作
-      let { data: res } = await this.$http.delete(`goods/${id}`)
+      let { data: res } = await this.$http.delete(`goods/${id}`);
       if (res.meta.status !== 200) {
-        return this.$message.error('删除失败!')
+        return this.$message.error("删除失败!");
       }
-      this.getGoodsList()
-      this.$message.success('删除成功!')
+      this.getGoodsList();
+      this.$message.success("删除成功!");
     },
     // 点击添加按钮
     goAddPage() {
-      this.$router.push('/goods/add')
+      this.$router.push("/goods/add");
+    },
+    // 点击编辑按钮
+    async showEditDialog(goods_id) {
+      let { data: res } = await this.$http.get(`goods/${goods_id}`);
+      if (res.meta.status !== 200) {
+        return this.$message.error("获取商品失败!");
+      }
+      this.editGood = res.data;
+      this.editDialogVisible = true;
+    },
+    // 关闭弹窗
+    editDialogClose() {
+      this.$refs.editGoodRef.resetFields();
+    },
+    // 提交编辑
+    submitEditGood() {
+      this.$refs.editGoodRef.validate(async (flag) => {
+        if (!flag) {
+          return;
+        }
+        // 验证通过,执行提交操作
+        let { data: res } = await this.$http.put(
+          `goods/${this.editGood.goods_id}`,
+          this.editGood
+        );
+        if (res.meta.status !== 201) {
+          this.$message.error("修改商品信息失败!");
+        }
+        this.getGoodsList();
+        this.$message.success("修改商品信息成功!");
+        this.editDialogVisible = false;
+      });
     },
   },
   created() {
-    this.getGoodsList()
+    this.getGoodsList();
   },
-}
+};
 </script>
 <style lang="less" scoped>
 </style>
--- 04_vue-shop/src/main.js
+++ 04_vue-shop/src/main.js
@@ -17,6 +17,14 @@ Vue.use(Element)
 import ZKTable from 'vue-table-with-tree-grid'
 Vue.component('tree-table', ZKTable)
 
+// 导入富文本编辑器
+import VueQuillEditor from 'vue-quill-editor'
+import 'quill/dist/quill.core.css' // import styles
+import 'quill/dist/quill.snow.css' // for snow theme
+import 'quill/dist/quill.bubble.css' // for bubble theme
+
+Vue.use(VueQuillEditor, /* { default global options } */ )
+
 // 引入axios
 import axios from 'axios'
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
