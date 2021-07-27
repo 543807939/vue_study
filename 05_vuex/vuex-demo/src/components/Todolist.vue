4c7249ed5a3fe317d32a075d9c8c2b56e4141edb
543807939@qq.com <543807939@qq.com>
2021-07-27 22:08:08 +0800

    study

--- 05_vuex/vuex-demo/src/components/Todolist.vue
+++ 05_vuex/vuex-demo/src/components/Todolist.vue
@@ -1,53 +1,93 @@
 
 <template>
   <div id="app">
-    <a-input placeholder="请输入任务" :value="inputValue" @change="inputValueChange" class="my_ipt" />
+    <a-input
+      placeholder="请输入任务"
+      :value="inputValue"
+      @change="inputValueChange"
+      class="my_ipt"
+    />
     <a-button type="primary" @click="addItemToList">添加事项</a-button>
 
-    <a-list bordered :dataSource="list" class="dt_list">
+    <a-list bordered :dataSource="infoList" class="dt_list">
       <a-list-item slot="renderItem" slot-scope="item">
         <!-- 复选框 -->
-        <a-checkbox :checked="item.done">{{item.info}}</a-checkbox>
+        <a-checkbox
+          :checked="item.done"
+          @change="checkboxStatusChanged($event, item.id)"
+          >{{ item.info }}</a-checkbox
+        >
         <!-- 删除链接 -->
-        <a slot="actions">删除</a>
+        <a slot="actions" @click="removeItemById(item.id)">删除</a>
       </a-list-item>
 
       <!-- footer区域 -->
       <div slot="footer" class="footer">
         <!-- 未完成的任务个数 -->
-        <span>0条剩余</span>
+        <span>{{$store.getters.restNum}}条剩余</span>
+        <span>{{restNum}}条剩余</span>
         <!-- 操作按钮 -->
         <a-button-group>
-          <a-button type="primary">全部</a-button>
-          <a-button>未完成</a-button>
-          <a-button>已完成</a-button>
+          <a-button :type="currentKey=='all'?'primary':''" @click="changeList('all')">全部</a-button>
+          <a-button :type="currentKey=='undone'?'primary':''" @click="changeList('undone')">未完成</a-button>
+          <a-button :type="currentKey=='done'?'primary':''" @click="changeList('done')">已完成</a-button>
         </a-button-group>
         <!-- 把已经完成的任务清空 -->
-        <a>清除已完成</a>
+        <a @click="clean">清除已完成</a>
       </div>
     </a-list>
   </div>
 </template>
 
 <script>
-import { mapState } from 'vuex'
+import { mapState,mapGetters } from "vuex";
 export default {
-  name: 'app',
+  name: "app",
   data() {
-    return {}
+    return {
+      active:'all'
+    };
   },
   computed: {
-    ...mapState(['list', 'inputValue']),
+    ...mapState([ "inputValue","currentKey"]),
+    ...mapGetters(['restNum','infoList'])
   },
   created() {
-    this.$store.dispatch('getList')
+    this.$store.dispatch("getList");
   },
   methods: {
+    // 输入框的值改变 改变vuex state中的值
     inputValueChange(e) {
-      this.$store.commit('setInputValue', e.target.value)
+      this.$store.commit("setInputValue", e.target.value);
     },
+    // 点击添加按钮,调用vuex mutations中的方法,将输入框的值添加到list中
+    addItemToList() {
+      if (this.inputValue.trim().length <= 0) {
+        return this.$message.warning("文本框内容不能为空！");
+      }
+      this.$store.commit("addItemToList", this.inputValue.trim());
+    },
+    // 点击删除 调用vuex mutations中的方法
+    removeItemById(id) {
+      this.$store.commit("removeItemById", id);
+    },
+    // 复选框的状态改变
+    checkboxStatusChanged(e, id) {
+      this.$store.commit("changeItemStatusById", {
+        id,
+        status: e.target.checked,
+      });
+    },
+    // 清除已完成的数据
+    clean(){
+      this.$store.commit('clean')
+    },
+    // 切换现实状态
+    changeList(flag){
+      this.$store.commit('changeCurrentKey',flag)
+    }
   },
-}
+};
 </script>
 
 <style scoped>
--- 05_vuex/vuex-demo/src/store/index.js
+++ 05_vuex/vuex-demo/src/store/index.js
@@ -6,7 +6,10 @@ Vue.use(Vuex)
 export default new Vuex.Store({
   state: {
     list: [],
-    inputValue: 'aa'
+    inputValue: '',
+    // 下一个节点的id
+    nextId: 5,
+    currentKey: 'all'
   },
   mutations: {
     initList(state, list) {
@@ -15,14 +18,62 @@ export default new Vuex.Store({
     // 为store中的inputValue赋值
     setInputValue(state, value) {
       state.inputValue = value
+    },
+    // 将inputValue添加到list中
+    addItemToList(state, task) {
+      if (state.inputValue) {
+        let obj = {
+          id: state.nextId,
+          info: task,
+          done: false
+        }
+        this.state.list.push(obj)
+        state.nextId++
+        state.inputValue = ''
+      }
+    },
+    // 根据id删除list中的元素
+    removeItemById(state, id) {
+      let index = state.list.findIndex(item => item.id == id)
+      state.list.splice(index, 1)
+    },
+    // 根据id修改状态
+    changeItemStatusById(state, params) {
+      let index = state.list.findIndex(item => item.id == params.id)
+      if (index !== -1) {
+        state.list[index].done = params.status
+      }
+    },
+    // 清除已完成的数据
+    clean(state) {
+      state.list = state.list.filter(item => item.done == false)
+    },
+    // 改变当前的视图状态
+    changeCurrentKey(state, key) {
+      state.currentKey = key
     }
   },
   actions: {
     async getList(context) {
-      let { data } = await axios.get('/list.json')
+      let {
+        data
+      } = await axios.get('/list.json')
       context.commit('initList', data)
     }
   },
-  modules: {
-  }
+  getters: {
+    restNum(state) {
+      return state.list.filter(item => item.done == false).length
+    },
+    infoList(state) {
+      if (state.currentKey == 'all') {
+        return state.list
+      } else if (state.currentKey == 'undone') {
+        return state.list.filter(item => item.done == false)
+      } else {
+        return state.list.filter(item => item.done == true)
+      }
+    }
+  },
+  modules: {}
 })
